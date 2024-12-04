d5343d7190340bcd830d3811a7c431b6370c875b
A2P1 <pabloazpitartegutierrez@gmail.com>
2024-12-04 12:36:50 +0100

    Opcion 1 terminada satisfactoriamente

--- PracticaBiblio/main.py
+++ PracticaBiblio/main.py
@@ -1,6 +1,6 @@
 #import numpy as np
 #lista_nombre = [] # type: list
-diccionario = {}
+diccionario = [] # Creo un diccionario vacio para almacenar libros
 def main():
     opcion = 0
     while opcion != 6:
@@ -26,13 +26,22 @@ def Menu():
     return opcion
 
 def Agregar():
-    titulo = input("Ingrese el título del libro: ")
-    autor = input("Ingrese el autor del libro: ")
-    año = int(input("Ingrese el año de publicación del libro: "))
+    while True:
+        
+        libros = {} #Creo el diccionario vacío para introducir los datos del libro
+        
+        titulo = input("Ingrese el título del libro: ")
+        autor = input("Ingrese el autor del libro: ")
+        año = int(input("Ingrese el año de publicación del libro: "))
+        
+        libros["titulo"] = titulo
+        libros["autor"] = autor
+        libros["año"] = año
+        diccionario.append(libros) #Añado el diccionario que contie los datos del libro al diccionario principal
+        continuar = input("Desea agregar otro libro? (S/N): ").upper() #Pregunto si desea agregar otro libro
+        if continuar != "S": # SI no quiere agregar otro libro, salgo del bucle
+            break
     
-    diccionario["titulo"] = titulo
-    diccionario["autor"] = autor
-    diccionario["año"] = año
     
 def Prestar():
     print("Ha elegido la opcion 2")
@@ -41,9 +50,10 @@ def Devolver():
     print("Ha elegido la opcion 3")
     return
 def Mostrar():
-    for clave, valor in diccionario.items():
-        print(f"{clave}: {valor}")
-    return
+   ''' for i, libro in enumerate(diccionario, start=1):
+        print(f"Libro {i}")
+        for clave, valor in libro.items():
+            print(f"{clave}: {valor}")'''
     
 def Buscar():
     
