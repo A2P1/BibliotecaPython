202933502cbc8ef5bebfd6b3bbd1b4f83c242ffb
A2P1 <pabloazpitartegutierrez@gmail.com>
2024-12-09 01:46:36 +0100

    Función para registrar a un usuario en el sistema terminada

--- PracticaBiblio/main.py
+++ PracticaBiblio/main.py
@@ -3,7 +3,11 @@
 import json
 diccionario = [] # Creo un diccionario vacio para almacenar libros
 diccionario_aux = [] #Creo un diccinario auxiliar para almacenar los libros prestados
+diccionario_usuario = [] #Diccionario vacío para guardar la informacion de los usuarios
+
 def main():
+    cont = 0
+    Registro(cont)
     opcion = 0
     while opcion != 7:
         opcion = Menu()
@@ -25,7 +29,7 @@ def main():
 def Menu():
     print("Bienvenido a la Biblioteca de Practica")
     print("Elija una opcion")
-    print("1. Agregar libro\n2. Prestar libro\n3. Devolver libro\n4. Mostrar libros disponibles\n5. Buscar libro\n6. Salir")
+    print("1. Agregar libro\n2. Prestar libro\n3. Devolver libro\n4. Mostrar libros disponibles\n5. Buscar libro\n6. Mandar datos a fichero\n7. Salir")
     opcion = int(input("Opcion: "))
     return opcion
 
@@ -44,7 +48,7 @@ def Agregar():
         libros["autor"] = autor
         libros["año"] = año
         
-        diccionario.append(libros) #Añado el diccionario que contie los datos del libro al diccionario principal
+        diccionario.append(libros) #Añado el diccionario que contiene los datos del libro al diccionario principal
         '''with open("Libros.json","a") as file: #crea un fichero llamado Libros.json
             json.dump(libros, file) #Guarda la informacion creada en el fichero'''
         continuar = input("Desea agregar otro libro? (S/N): ").upper() #Pregunto si desea agregar otro libro
@@ -105,6 +109,30 @@ def Buscar():
 def MandarFichero():
     with open("Libros.json", "w") as file: #Edito el archivo Libros.json para incluir el diccionario
         json.dump(diccionario, file, indent=3) #Guardo la información en el archivo poniendo que almacene en cada objeto 3 datos
+        
+
+
+
+def Registro(cont):
+    lista = {} #Creo una lista vacía para alamcenar los datos de usuario y su contraseña
+    usuario = input("Ingrese un nombre del usuario: ")
+    contraseña = input("Ingrese una contrasena:")
+    contraseña2 = input("Vuelva a ingresar la contrasena: ")
+    
+    if contraseña != contraseña2: #Compruebo que las contraseñas coinciden, si no, vuelvo a registrar
+        print("Las contrasenas no coinciden")
+        cont += 1
+        print(cont)
+        if cont == 3: #Si falla 3 veces, se sale del programa
+            Salir()
+        Registro(cont)
+    else:
+        lista["Usuario"] = usuario # Almaceno los datos en la lista
+        lista["Contrasena"] = contraseña
+        diccionario_usuario.append(lista) # Añado los datos al diccionario
+        with open("Usuarios.json", "w") as file:
+            json.dump(diccionario_usuario, file, indent=2) #Escribo el fichero Usuarios.json para añadir los datos del usuario
+            
 def Salir():
     exit()
 def normalizar(texto):
